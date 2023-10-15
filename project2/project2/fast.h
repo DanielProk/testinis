b1c1b71fd7259783dd24eb79a89439fafafc1d98
DanielProk <145119073+DanielProk@users.noreply.github.com>
2023-10-15 15:49:54 +0300

    Add files via upload

---
+++ project2/project2/fast.h
@@ -0,0 +1,13 @@
+#pragma once
+#pragma once
+#include <iostream>
+#include <iomanip>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fstream>
+#include <sstream> 
+#include <vector> 
+#include <chrono>
+//#include <boost/algorithm/string.hpp>
+
+void bufer_nusk(std::string read_vardas, std::string write_vardas);
